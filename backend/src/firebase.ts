6a612bb51aec7d0729b257e26b6476cb756f8e16
Yamil Ali <Yamil8787@gmail.com>
2026-06-08 20:00:16 -0300

    Fix firebase config for production

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -1,8 +1,11 @@
 import admin from "firebase-admin";
-import serviceAccount from "../chatroom-d0143-firebase-adminsdk-fbsvc-5935d83412.json";
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  }),
   databaseURL: "https://chatroom-d0143-default-rtdb.firebaseio.com",
 });
 
